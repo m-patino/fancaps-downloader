92342956b285f31d5eaac4cfde8485b45dc2a715
Maxime <m.caudrelier@outlook.fr>
2024-04-01 18:43:02 +0200

    episode crawler

--- scraper/crawlers/episode_crawler.py
+++ scraper/crawlers/episode_crawler.py
@@ -1,6 +1,45 @@
+import re
+from bs4 import BeautifulSoup
+import urllib.request
+
 class EpisodeCrawler:
     url = None
+    alt = None
 
     def crawl(self, url):
+        picLinks = []
         self.url = url
-        return [url]
+        currentUrl = self.url
+        pageNumber = 1
+
+        # get Episode type for setup  cdn and regex
+        match = re.search(r"https://fancaps.net/(.*?)/(.*)", url)
+        epType = match.group(1)
+        nextUrl = match.group(2)
+
+        if epType == 'tv':
+            cdn = 'tvcdn'
+        else:
+            cdn = 'ancdn'
+
+        while currentUrl:
+            request = urllib.request.Request(currentUrl, headers={'User-Agent': 'Mozilla/5.0'})
+            page = urllib.request.urlopen(request)
+            beautifulSoup = BeautifulSoup(page, "html.parser")
+
+            for img in beautifulSoup.find_all("img", src=re.compile("^https://"+epType+"thumbs.fancaps.net/")):
+                imgSrc = img.get("src")
+                imgAlt = img.get("alt")
+                if not self.alt:
+                    self.alt = imgAlt
+                if self.alt == imgAlt:
+                    picLinks.append(imgSrc.replace("https://"+epType+"thumbs.fancaps.net/", "https://"+cdn+".fancaps.net/"))
+            next = nextUrl+f"&page={pageNumber + 1}"
+            nextPage = beautifulSoup.find("a", href=next)
+            if nextPage:
+                pageNumber += 1
+                currentUrl = url + f"&page={pageNumber}"
+            else:
+                currentUrl = None
+        
+        return picLinks
